e7ec4de6ab1dedf5d45359c2c5675f37de5ee4a1
sapanb <sapanb@8c455092-636d-4788-adf5-e71def0336e8>
2008-09-25 08:21:26 +0000

    Call GetSlices() and cache it locally.

---
+++ fast_get_slices.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+import xmlrpclib
+import sys
+import MySQLdb
+
+def format(slicename):
+	return ("<a href=\"mailto:%s@slices.planet-lab.org?cc=support@planet-lab.org\">%s</a>" % (slicename,slicename))
+
+if __name__ == "__main__":
+	sliceid=int(sys.argv[1])
+	conn = MySQLdb.connect (host = "localhost",
+				user = "root",
+				passwd = "foobar",
+				db = "planetflow")
+
+	cursor = conn.cursor ()
+	cursor.execute ("SELECT name FROM slices WHERE id=%s" % sys.argv[1])
+	row = cursor.fetchone ()
+	if ( sliceid==0 ):
+		link = format('root')
+	elif ( not row ):
+		plcapi = "https://boot.planet-lab.org/PLCAPI/"
+		s = xmlrpclib.ServerProxy(plcapi, allow_none=True)
+		auth = dict(AuthMethod='password',Username='',AuthString='')
+		
+		slices=s.GetSlices(auth,{'slice_id':sliceid})
+		if (len(slices)>0):
+			sliceid_str="%s"%sliceid
+			slicename=slices[0]['name']
+			insertcmd='INSERT INTO slices (id,name,descr) VALUES (%s,\'%s\',\'%s\')'%(sliceid_str,slicename,'')
+			cursor.execute(insertcmd)
+			link = format(slices[0]['name'])
+		else: 
+			link = "Id %d" % sliceid
+	else:
+		link = format(row[0])
+		
+	print link
+

94a151831df01a6c12f6eb7fd61196d50ccdbc05
sapanb <sapanb@8c455092-636d-4788-adf5-e71def0336e8>
2009-04-02 19:50:14 +0000

    More cleanups

--- datasync.py
+++ datasync.py
@@ -43,7 +43,7 @@ class DataSyncThread(threading.Thread):
 	def start_download (self):
 		local_tmpdir = "%s/%s" % (globals.rawdatadir,self.ip)
 		for remote_path in globals.paths:
-			rsync_command = "rsync --timeout 30 -avzu %s@%s:%s/pf* %s"%(self.slice_name,self.ip,
+			rsync_command = "rsync --timeout 30 -avzu %s@%s:%s/* %s"%(self.slice_name,self.ip,
 			                                              remote_path,local_tmpdir)
 			logger.l.info(rsync_command)
 

f6c1e24bffdc509d429533047319f53875f31f7c
sapanb <sapanb@8c455092-636d-4788-adf5-e71def0336e8>
2010-05-06 16:09:05 +0000

    Added some notes for people trying to bring up their own PFC.

--- pdelta/trunk/globals.py
+++ pdelta/trunk/globals.py
@@ -2,9 +2,41 @@
 
 import re
 
-plprefix='pl'
-# plprefix='ple'
+# PFC-specific variables
+# If you are bringing up your own PFC, then you should only
+# need to configure the variables below
 
+# Change the following to the prefix you set when you ran
+# plc-tty-config
+plprefix='pl'
+
+# 'accountdir' points to a directory that pdelta uses to dump statistics
+# about the data collected. As long as you set this to a directory writable by pdelta, you should be fine
+accountdir="/usr/local/pdelta-production-4.0/accounting"
+
+# The following is a 'last-resort' hack for dealing with situations in which 
+# the planetflow slice is not set up properly on a node. If you think your plprefix_netflow slice is guaranteed to be set up properly, then feel free to set the following to an empty dict
+
+path_hook = { "/pf":"if [ ! -e /etc/init.d/pf2slice ]; then wget www.cs.princeton.edu/~sapanb/pf2slice;sudo mkdir -p /pf;sudo mv pf2slice /etc/init.d;sudo chmod +x /etc/init.d/pf2slice;sudo /sbin/chkconfig --add pf2slice;sudo /sbin/service pf2slice start; fi" }
+# path_hook = {}
+
+# List of PLCs you would like to gather data from, with the authentication tokens set 
+plcaccess = [("https://boot.planet-lab.org/PLCAPI/","pl",dict(AuthMethod='password',Username='gwsapan@gmail.com',AuthString='')),("https://www.planet-lab.eu/PLCAPI/","ple",dict(AuthMethod='anonymous'))]
+
+# Silk directories. For pdelta to work, just make sure that these are writeable.
+silkpath = "/usr/local/pdelta-production-4.0/local/"
+rawdatadir = "/usr/local/pdelta-production-4.0/data_raw"
+silkdatadir= "/usr/local/pdelta-production-4.0/data_import"
+
+
+###
+###
+###
+### Advanced - please read the source code before modifying these
+
+
+debug_mask = re.compile(r'^/tmp/mnt/sysimg')
+paths = [ "/pf","/tmp/mnt/sysimg/var/local/fprobe","/var/local/fprobe" ]
 do_bw_limit = False
 max_bandwidth = 10                                        # in megaoctects/second. b/w cap on PFC
 max_concurrency = 13                                       # in megaoctects/second. b/w cap on PFC
@@ -14,16 +46,6 @@ starttime = 0.0
 
 node_count_prod = 0
 node_count_deb = 0
-accountdir="/usr/local/pdelta-production-4.0/accounting"
-paths = [ "/pf","/tmp/mnt/sysimg/var/local/fprobe","/var/local/fprobe" ]
-path_hook = { "/pf":"if [ ! -e /etc/init.d/pf2slice ]; then wget www.cs.princeton.edu/~sapanb/pf2slice;sudo mkdir -p /pf;sudo mv pf2slice /etc/init.d;sudo chmod +x /etc/init.d/pf2slice;sudo /sbin/chkconfig --add pf2slice;sudo /sbin/service pf2slice start; fi" }
-debug_mask = re.compile(r'^/tmp/mnt/sysimg')
-
-
-plcaccess = [("https://boot.planet-lab.org/PLCAPI/","pl",dict(AuthMethod='password',Username='gwsapan@gmail.com',AuthString='')),("https://www.planet-lab.eu/PLCAPI/","ple",dict(AuthMethod='anonymous'))]
-silkpath = "/usr/local/pdelta-production-4.0/local/"
-rawdatadir = "/usr/local/pdelta-production-4.0/data_raw"
-silkdatadir= "/usr/local/pdelta-production-4.0/data_import"
 
 # You may set one, the other, or neither.  But, not both.
 nodegroup_exclude = None
